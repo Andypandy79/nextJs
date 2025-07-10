0cce6c61acfb7dad7ee2d9869cda32a117eea531
Andypandy79 <78705049+Andypandy79@users.noreply.github.com>
2025-07-10 21:35:37 +0200

    fix

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import {hashSync} from 'bcrypt-ts-edge';
 
 
 const sampleData = {
   users: [
-=======
-import { hashSync } from 'bcrypt-ts-edge';
-
-const sampleData = {
-   users: [
->>>>>>> 5d287da8301694acd5694b0e22c6a76cd78a4080
     {
       name: 'John',
       email: 'admin@example.com',
@@ -23,10 +16,6 @@ const sampleData = {
       role: 'user',
     },
   ],
-<<<<<<< HEAD
-=======
-
->>>>>>> 5d287da8301694acd5694b0e22c6a76cd78a4080
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',

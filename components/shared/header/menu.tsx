f185e458b3f45d9e95163125e89225cd916e57d6
Andypandy79 <78705049+Andypandy79@users.noreply.github.com>
2025-07-10 21:18:41 +0200

    fix2

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -7,8 +7,8 @@ import UserButton from "./user-button";
 
 const Menu = () => {
   return ( 
-    <div className="flex">
-      <nav className='hidden md:flex w-full max-w-xs gap-1'>
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-1">
         <ModeToggle />
         <Button asChild variant='ghost'>
           <Link href='/cart'>
